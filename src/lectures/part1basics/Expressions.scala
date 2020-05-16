d6490fcf22e2230093975ec76c0dddaf815fb51e
memowii <memogl25@gmail.com>
2020-05-16 15:27:03 -0500

    Expressions

---
+++ src/lectures/part1basics/Expressions.scala
@@ -0,0 +1,67 @@
+package lectures.part1basics
+
+import jdk.nashorn.internal.ir.LiteralNode.PrimitiveLiteralNode
+
+object Expressions extends App {
+  val x = 1 + 2 // EXPRESSION
+  println(x)
+
+  println(2 + 3 * 4)
+  // + - * / & << >> >>> (right shift with zero extension)
+
+  println(1 == x)
+  // == != > >= < <=
+
+  println(!(1 == x))
+  // ! && ||
+
+  var aVariable = 2
+  aVariable += 3 // also works with -= *= /= ... side effects
+  println(aVariable)
+
+  // Instructions (DO) vs Expressions (VALUE)
+  // IF expression
+  val aCondition = true
+  val aConditionedValue = if (aCondition) 5 else 3 // IF EXPRESSION
+  println(aConditionedValue)
+  println(if (aCondition) 5 else 3)
+  println(1 + 3)
+
+  var i = 0
+  val aWhile = while (i < 10) {
+    println(i)
+    i += 1
+  }
+  // NEVER WRITE THIS AGAIN
+  // EVERYTHING in Scala is an Expression!
+
+  val aWeirdValue = (aVariable = 3) // Unit === void
+  println(aWeirdValue)
+
+  // side effects: println(), whiles, reassigning
+
+  // Ccode blocks
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if (z > 2) "hello" else "goodbye"
+  }
+
+  println(aCodeBlock)
+
+  // 1. difference between "hello world" and println("hello world")?
+  // "hello world" is string, an expression
+  // println("hello world") is instruction and it creates a side effect, thus returns unit
+
+  val someValue = {
+    2 < 3
+  }
+  println(someValue)
+
+  val someOtherValue = {
+    if (someValue) 239 else 986
+    42
+  }
+  println(someOtherValue)
+}
