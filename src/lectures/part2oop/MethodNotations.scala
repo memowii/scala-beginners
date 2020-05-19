5821358ae32a3b08391be5a15029439be6a6231c
memowii <memogl25@gmail.com>
2020-05-19 16:46:05 -0500

    Method Notations (Exercises)

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,12 +2,17 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
     def unary_! : String = s"$name, what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times"
+    def learns(subject: String) = s"$name learns $subject"
+    def learnsScala = this learns "Scala"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -41,4 +46,21 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary()) // equivalent
+
+  val maryRocks = mary + "The rockstar"
+  println(maryRocks.name)
+
+  val maryAge = +mary
+  println(maryAge.age)
+
+  println(mary learns "English")
+
+  println(mary learnsScala)
+
+  println(mary(5))
+
+  println((mary + "the Rockstar").apply())
+  println((+mary).age)
+  println(mary learnsScala)
+  println(mary(10))
 }
